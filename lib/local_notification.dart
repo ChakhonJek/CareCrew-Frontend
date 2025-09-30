967e29af7ae4118706c3119aa53caf1c80ba3d73
ชาคร ไสยโสภณ <chakhon.si@rmuti.ac.th>
2025-09-30 07:20:45 +0700

    use icon of app

--- lib/local_notification.dart
+++ lib/local_notification.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/material.dart';
 
 class LocalNotification {
   static final FlutterLocalNotificationsPlugin _notificationsPlugin =
@@ -8,7 +7,7 @@ class LocalNotification {
 
   static Future<void> init() async {
     const AndroidInitializationSettings androidSettings =
-    AndroidInitializationSettings('carecrew');
+    AndroidInitializationSettings('ic_launcher');
 
     const InitializationSettings initSettings =
     InitializationSettings(android: androidSettings, iOS: DarwinInitializationSettings());
